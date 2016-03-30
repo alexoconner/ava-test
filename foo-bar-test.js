20fc92aacc42883084f94b22d1caec94ddd3104d
alexoconner <alexander@skrabl.de>
2016-03-30 11:24:03 +0100

    writing simple test case with ava

--- foo-bar-test.js
+++ foo-bar-test.js
@@ -5,8 +5,7 @@ export default function fooBarTest( numbers ) {
      * check for array
      */
     if ( !Array.isArray(numbers) ) {
-        console.error('numbers must be an array');
-        return false;
+        throw new Error('numbers must be an array');
     }
 
     /**
@@ -14,21 +13,24 @@ export default function fooBarTest( numbers ) {
      */
     numbers.forEach( ( val, key ) => {
         if ( !Number.isInteger(val)) {
-            console.error('array must contain integers only');
+            throw new Error('array must contain integers only');
         }
     });
 
     // if all is good above map the list and return a new one
-    return numbers.map( ( val, key ) {
+    return numbers.map( ( val, key ) => {
+        if ( val % 3 === 0 && val % 5 === 0 ) {
+            return 'quux';
+        }
         if ( val % 3 === 0 ) {
             // number can be divided by 3
             return 'foo';
         }
-        if ( val % 3 === 0 ) {
+        if ( val % 5 === 0 ) {
             // number can be divided by 5
             return 'bar';
         }
-
+        return val;
     });
 
 }
--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 
 import test from 'ava';
+import fooBarTest from './foo-bar-test';
 
 test('my first test', t => {
-    t.pass();
+
+    t.plan(2);
+
+    let bohho = [3, 5];
+    let result = fooBarTest(bohho);
+    let expected = ['foo', 'bar'];
+
+    t.same( result, expected, 'test' );
+    t.same( fooBarTest([5,10,15]), ['bar', 'bar', 'bar'], 'test' );
+
 });
